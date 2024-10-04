753e3b0fc87a083560fc62c4eb3c75a0c1076638
Elmer Nocon <elmernocon@gmail.com>
2024-10-04 11:18:02 +0800

    test(local-hosted): fix test

--- test/sample/test-local-hosted.sh
+++ test/sample/test-local-hosted.sh
@@ -14,7 +14,6 @@ function clean_up()
 trap clean_up EXIT
 
 echo '# Build and run Extend app locally'
-python -m pip install -r requirements.txt
 (cd src && python -m app)& GRPC_SERVER_PID=$!
 
 (for _ in {1..12}; do bash -c "timeout 1 echo > /dev/tcp/127.0.0.1/8080" 2>/dev/null && exit 0 || sleep 5s; done; exit 1)
